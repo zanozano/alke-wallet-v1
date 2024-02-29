1521879b964b836cab82066b25927dcf8262483b
Cristobal Andres Manzano Martinez <zano@live.cl>
2024-02-29 14:54:07 +0700

    update

--- src/db/data.sql
+++ src/db/data.sql
@@ -13,6 +13,9 @@ CREATE TABLE credit_accounts (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID UNIQUE,
     balance DECIMAL(15, 2) NOT NULL DEFAULT 0,
+    credit_card_number VARCHAR(16),
+    expiration_date DATE,
+    cvv VARCHAR(3),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 --
@@ -20,6 +23,9 @@ CREATE TABLE savings_accounts (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID UNIQUE,
     balance DECIMAL(15, 2) NOT NULL DEFAULT 0,
+    credit_card_number VARCHAR(16),
+    expiration_date DATE,
+    cvv VARCHAR(3),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 --
@@ -44,3 +50,73 @@ CREATE TABLE savings_transactions (
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (savings_account_id) REFERENCES savings_accounts(id)
 );
+--
+INSERT INTO users (first_name, last_name, email, password)
+VALUES (
+        'Cristobal',
+        'Manzano',
+        'cristobal@alkewallet.com',
+        '123456'
+    );
+--
+SELECT id INTO new_user_id
+FROM users
+ORDER BY id DESC
+LIMIT 1;
+--
+INSERT INTO credit_accounts (
+        user_id,
+        balance,
+        credit_card_number,
+        expiration_date,
+        cvv
+    )
+SELECT u.id AS user_id,
+    100000 AS balance,
+    (
+        SELECT STRING_AGG(FLOOR(RANDOM() * 10)::TEXT, '')
+        FROM GENERATE_SERIES(1, 16)
+    ) AS credit_card_number,
+    CURRENT_DATE + INTERVAL '1 year' * (FLOOR(RANDOM() * 10) + 1) AS expiration_date,
+    (
+        SELECT STRING_AGG(FLOOR(RANDOM() * 10)::TEXT, '')
+        FROM GENERATE_SERIES(1, 3)
+    ) AS cvv
+FROM users u;
+INSERT INTO savings_accounts (
+        user_id,
+        balance,
+        credit_card_number,
+        expiration_date,
+        cvv
+    )
+SELECT u.id AS user_id,
+    100000 AS balance,
+    (
+        SELECT STRING_AGG(FLOOR(RANDOM() * 10)::TEXT, '')
+        FROM GENERATE_SERIES(1, 16)
+    ) AS credit_card_number,
+    CURRENT_DATE + INTERVAL '1 year' * (FLOOR(RANDOM() * 10) + 1) AS expiration_date,
+    (
+        SELECT STRING_AGG(FLOOR(RANDOM() * 10)::TEXT, '')
+        FROM GENERATE_SERIES(1, 3)
+    ) AS cvv
+FROM users u;
+--
+SELECT u.id AS user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    c.id AS credit_account_id,
+    c.balance AS credit_balance,
+    c.credit_card_number AS credit_card,
+    c.expiration_date AS credit_expiration_date,
+    c.cvv AS credit_cvv,
+    s.id AS savings_account_id,
+    s.balance AS savings_balance,
+    s.credit_card_number AS savings_credit_card,
+    s.expiration_date AS savings_expiration_date,
+    s.cvv AS savings_cvv
+FROM users u
+    LEFT JOIN credit_accounts c ON u.id = c.user_id
+    LEFT JOIN savings_accounts s ON u.id = s.user_id;
